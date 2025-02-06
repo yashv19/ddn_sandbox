c01db0e2f467aee8be9497995a16f8df495cae10
Yash Vedanaparti <yvedanaparti@gmail.com>
2025-02-06 15:45:40 -0500

    snowflake connected

--- cinema/connector/ts_modifier/functions.ts
+++ cinema/connector/ts_modifier/functions.ts
@@ -1,6 +1,15 @@
+
 /**
- * @readonly Exposes the function as an NDC function (the function should only query data without making modifications)
+ * @readonly Exposes the function as an NDC function (the function should only query data without making modifications). Returns upper case
  */
-export function hello(name?: string) {
-  return `hello ${name ?? "world"}`;
+export function get_uppercase(name?: string) {
+  return `${name?.toUpperCase()}`
+}
+
+
+/**
+ * This is exposed as a command. Returns lower case
+ */
+export function get_lowercase(name?: string) {
+  return `${name?.toLowerCase()}`
 }
